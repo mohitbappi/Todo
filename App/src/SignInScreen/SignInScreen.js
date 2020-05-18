0e5b96d00374c4b8c698119e708ad9c0d73898c0
mohit.agrawal <mohit.agrawal@unthinkable.co>
2020-05-18 09:48:44 +0530

    First commit

---
+++ App/src/SignInScreen/SignInScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignInScreen extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Hi</Text>
+            </View>
+        );
+    }
+}
+
+export default SignInScreen;
